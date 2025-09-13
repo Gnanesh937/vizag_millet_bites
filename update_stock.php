4e4ff8a3f3c9d863a84aee0f310868a1f37c7dba
Gnanesh937 <gnaneswarasam@gmail.com>
2025-09-13 13:37:38 +0530

    Create update_stock.php

---
+++ update_stock.php
@@ -0,0 +1,9 @@
+
+<?php
+include 'db.php';
+if(isset($_GET['id'], $_GET['status'])){
+    $id = $_GET['id'];
+    $status = $_GET['status'];
+    $conn->query("UPDATE products SET stock_status='$status' WHERE id=$id");
+}
+?>
