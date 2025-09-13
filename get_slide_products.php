b9655cbf46739446bc117e5f237a8f04b553c1b9
Gnanesh937 <gnaneswarasam@gmail.com>
2025-09-13 14:58:03 +0530

    Delete get_slide_products.php

--- get_slide_products.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-include 'db.php';
-$res = $conn->query("SELECT * FROM products WHERE in_slide=1");
-$slides = [];
-while($row = $res->fetch_assoc()) $slides[] = $row;
-echo json_encode($slides);
-?>
