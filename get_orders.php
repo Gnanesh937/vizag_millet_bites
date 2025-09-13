5820989b2bf0818cf84f2c25d3be7677739f181e
Gnanesh937 <gnaneswarasam@gmail.com>
2025-09-13 13:31:02 +0530

    Create get_orders.php

---
+++ get_orders.php
@@ -0,0 +1,26 @@
+
+<?php
+include 'db.php';
+
+$res = $conn->query("SELECT * FROM orders ORDER BY id DESC");
+$orders = [];
+
+while($o = $res->fetch_assoc()){
+    $orderId = $o['id'];
+    $itemsRes = $conn->query("SELECT p.name, oi.quantity as qty, oi.price FROM order_items oi JOIN products p ON oi.product_id=p.id WHERE oi.order_id=$orderId");
+    $items = [];
+    while($i = $itemsRes->fetch_assoc()) $items[] = $i;
+
+    $orders[] = [
+        'id' => $o['id'],
+        'total_price' => $o['total_price'],
+        'payment_ref' => $o['payment_ref'],
+        'customer_name' => $o['customer_name'],
+        'customer_phone' => $o['customer_phone'],
+        'customer_address' => $o['customer_address'],
+        'items' => $items
+    ];
+}
+
+echo json_encode($orders);
+?>
